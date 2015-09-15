e72a3a15bb4e322e9a65370796a174c65b168fdd
Amit Kapadia <amit@planet.com>
2015-09-14 17:29:20 -0700

    fix timestamp format. fix selection of geojson attribute

--- sentinels/format.py
+++ sentinels/format.py
@@ -13,11 +13,11 @@ def feature(geojson):
         region of interest.
     """
 
-    if geojson.get('Type') == 'Feature':
+    if geojson.get('type') == 'Feature':
         geometry = geojson.get('geometry')
     else:
         geometry = geojson.get('features')[0].get('geometry')
-    
+
     c = geometry.get('coordinates')
     wkt = 'POLYGON ((%s))' % ', '.join([ "%.16f %.16f" % (x[0], x[1]) for x in c[0] ])
 
@@ -32,7 +32,7 @@ def time_interval(position):
     :param position:
         Tuple containing two datetime strings.
     """
-    time_range = map(lambda d: parse(d).strftime('%Y-%m-%dT%H:%M:%SZ'), position)
+    time_range = map(lambda d: parse(d).strftime('%Y-%m-%dT%H:%M:%S.%fZ'), position)
     return '[%s]' % ' TO '.join(time_range)
 
 
